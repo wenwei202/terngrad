8f41452c8a77ce07405cf4823b6e57f9ae36ebb9
Wei Wen <weiwen.web@gmail.com>
2017-04-12 11:46:41 -0400

    build all sh

---
+++ build_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+set -e
+
+bazel build inception/download_and_preprocess_imagenet
+
+bazel build inception/cifar10_train
+bazel build inception/cifar10_eval
+
+bazel build inception/imagenet_train
+bazel build inception/imagenet_eval
+
+bazel build inception/imagenet_distributed_train
+bazel build inception/cifar10_distributed_train
