8e08152969a1b6d91079d0d224dd89c6c0f7e351
Wei Wen <weiwen.web@gmail.com>
2017-09-27 10:30:23 -0400

    kill local

--- terngrad/kill_local.sh
+++ terngrad/kill_local.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-ps aux | grep python | grep distributed_train | grep ${USER} | awk '{print $2}' | xargs kill
+threadid=$( ps aux | grep python | grep distributed_train | grep ${USER} | awk '{print $2}')
+if [[ "$threadid" =~ ^-?[0-9]+$ ]] ; 
+then
+  kill $threadid
+else
+  echo "Stopped already."
+fi
