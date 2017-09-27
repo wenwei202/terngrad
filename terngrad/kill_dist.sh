bacca981da7cec87ad4b34169907b384c78acc0a
Wei Wen <weiwen.web@gmail.com>
2017-09-26 23:00:32 -0400

    stop all nodes

--- terngrad/kill_dist.sh
+++ terngrad/kill_dist.sh
@@ -1,2 +1,39 @@
 #!/bin/bash
-ps aux | grep python | grep distributed_train | grep ${USER} | awk '{print $2}' | xargs kill
+set -e
+#set -x
+
+bash ./dist_config.sh
+
+WORKER_NUM=${#WORKER_HOSTS[@]}
+DEVICE_NUM=${#WORKER_DEVICES[@]}
+DATA_NUM=${#DATA_DIR[@]}
+if [ ${WORKER_NUM} -ne ${DEVICE_NUM}  ]
+then
+  echo "The number of workers (${WORKER_NUM}) does not match the number of devices (${DEVICE_NUM})"
+  exit
+fi
+if [ ${WORKER_NUM} -ne ${DATA_NUM}  ]
+then
+  echo "The number of workers (${WORKER_NUM}) does not match the number of data paths (${DATA_NUM})"
+  exit
+fi
+
+
+# stop workers
+for HOST in ${WORKER_HOSTS[*]}; do
+  worker=$(echo ${HOST} |cut -d':' -f1)
+  ssh ${worker} "hostname; \
+    cd ${WORKSPACE}; \
+    pwd; \
+    ./kill_local.sh "
+done
+
+# stop ps
+for HOST in ${PS_HOSTS[*]}; do
+  ps=$(echo ${HOST} |cut -d':' -f1)
+  ssh ${ps} "hostname; \
+             cd ${WORKSPACE}; \
+             pwd; \
+             ./kill_local.sh "
+done
+
--- terngrad/run_dist.sh
+++ terngrad/run_dist.sh
@@ -4,6 +4,13 @@ set -e
 
 bash ./dist_config.sh
 
+WORKER_STRING=$(echo ${WORKER_HOSTS[*]} | sed 's/ /,/g') 
+PS_STRING=$(echo ${PS_HOSTS[*]} | sed 's/ /,/g') 
+EXPERIMENT_ID=$(date)
+EXPERIMENT_ID=${EXPERIMENT_ID// /_}
+EXPERIMENT_ID=${EXPERIMENT_ID//:/-}
+
+PS_NUM=${#PS_HOSTS[@]}
 WORKER_NUM=${#WORKER_HOSTS[@]}
 DEVICE_NUM=${#WORKER_DEVICES[@]}
 DATA_NUM=${#DATA_DIR[@]}
@@ -18,22 +25,28 @@ then
   exit
 fi
 
+echo "${PS_NUM} ps hosts: ${PS_STRING}"
+echo "${WORKER_NUM} worker hosts: ${WORKER_STRING}"
 
-# stop workers
+# start workers
+task_id=0
 for HOST in ${WORKER_HOSTS[*]}; do
   worker=$(echo ${HOST} |cut -d':' -f1)
   ssh ${worker} "hostname; \
     cd ${WORKSPACE}; \
     pwd; \
-    ./kill_local.sh "
+    ${WORKER_SCRIPT} ${PS_STRING} ${WORKER_STRING} worker ${task_id} ${WORKER_DEVICES[$task_id]} ${DATA_DIR[$task_id]} ${EXPERIMENT_ID}"
+  task_id=`expr $task_id + 1`
 done
 
-# stop ps
+# start ps
+task_id=0
 for HOST in ${PS_HOSTS[*]}; do
   ps=$(echo ${HOST} |cut -d':' -f1)
   ssh ${ps} "hostname; \
              cd ${WORKSPACE}; \
              pwd; \
-             ./kill_local.sh "
+             ${PS_SCRIPT} ${PS_STRING} ${WORKER_STRING} ps ${task_id} ${EXPERIMENT_ID}"
+  task_id=`expr $task_id + 1`
 done
 
