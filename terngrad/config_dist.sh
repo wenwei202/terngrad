cc8df550acb10de5ef9a113e78dbbddd1438350f
Wei Wen <weiwen.web@gmail.com>
2017-09-26 23:29:25 -0400

    comments of usages

--- terngrad/config_dist.sh
+++ terngrad/config_dist.sh
@@ -2,28 +2,50 @@
 set -e
 set -x
 
-# The executable path. Must be the same across all nodes
+
+
+################################################
+# The path of executables (terngrad/terngrad). Must be the same across all nodes
 WORKSPACE="~/github/users/wenwei202/terngrad/terngrad"
 
-#WORKER_SCRIPT="./run_single_worker_alexnet.sh"
-#PS_SCRIPT="./run_single_ps_imagenet.sh"
-WORKER_SCRIPT="./run_single_worker_cifarnet.sh"
-PS_SCRIPT="./run_single_ps_cifar10.sh"
 
+
+
+################################################
+# The script to start worker
+#WORKER_SCRIPT="./run_single_worker_alexnet.sh"
+WORKER_SCRIPT="./run_single_worker_cifarnet.sh"
+
+# The script to start ps (depending on dataset only)
+# Select from one of two below
+PS_SCRIPT="./run_single_ps_cifar10.sh"
+#PS_SCRIPT="./run_single_ps_imagenet.sh"
+
+
+
+
+################################################
+# The list of host:port of ps
+# Multiple ps not tested yet
 PS_HOSTS=( \
   10.236.176.29:2222 \
 )
+
+
+
+################################################
+# The list of host:port of workers
 WORKER_HOSTS=( \
   10.236.176.28:2224 \
   10.236.176.29:2226 \
 )
+# GPU IDs of corresponding workers
 WORKER_DEVICES=( \
   1 \
   2 \
 )
+# paths of dataset shards of corresponding workers
 DATA_DIR=( \
   ~/dataset/cifar10-data-shard-0-499 \
   ~/dataset/cifar10-data-shard-500-999 \
 )
-
-
