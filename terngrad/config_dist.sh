f47e9ec0e4bdbca86215d023216f4cd81e637b1e
Wei Wen <weiwen.web@gmail.com>
2017-09-27 11:16:10 -0400

    Update config_dist.sh

--- terngrad/config_dist.sh
+++ terngrad/config_dist.sh
@@ -17,15 +17,14 @@ WORKSPACE="~/github/users/wenwei202/terngrad/terngrad"
 WORKER_SCRIPT="./run_single_worker_cifarnet.sh"
 
 # The script to start ps (depending on dataset only)
-# Select from one of two below
+# Select one from those below
 PS_SCRIPT="./run_single_ps_cifar10.sh"
 #PS_SCRIPT="./run_single_ps_imagenet.sh"
 
 
 
-
 ################################################
-# The list of host:port of ps
+# The list of hosts and ports of ps
 # Multiple ps not tested yet
 PS_HOSTS=( \
   10.236.176.29:2222 \
@@ -34,17 +33,17 @@ PS_HOSTS=( \
 
 
 ################################################
-# The list of host:port of workers
+# The list of hosts and ports of workers
 WORKER_HOSTS=( \
   10.236.176.28:2224 \
   10.236.176.29:2226 \
 )
-# GPU IDs of corresponding workers
+# GPU IDs in corresponding workers
 WORKER_DEVICES=( \
   1 \
   2 \
 )
-# paths of dataset shards of corresponding workers
+# Paths of dataset shards in corresponding workers
 DATA_DIR=( \
   ~/dataset/cifar10-data-shard-0-499 \
   ~/dataset/cifar10-data-shard-500-999 \
